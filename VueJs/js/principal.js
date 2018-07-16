bde6e3f3d7b3f864fdcd82c71aac742f48027ee0
Ed <picassofb@hotmail.com>
2018-07-16 00:06:54 -0500

    Practicas Vue JS

---
+++ VueJs/js/principal.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el : '#app',
+    data : {
+        lista : [
+            {nombre: "Carlos", promedio: "10", estado: false},
+            {nombre: "Juan", promedio: "15", estado: true},
+            {nombre: "Miguel", promedio: "18", estado: true}
+        ],
+    }
+})
