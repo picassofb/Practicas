3245213a8d94556904003be0cbfbb7aa6eab44cb
Ed <picassofb@hotmail.com>
2018-07-17 23:24:46 -0500

    form nombre y promedio

--- VueJs/js/principal.js
+++ VueJs/js/principal.js
@@ -2,9 +2,25 @@ var app = new Vue({
     el : '#app',
     data : {
         lista : [
-            {nombre: "Carlos", promedio: "10", estado: false},
-            {nombre: "Juan", promedio: "15", estado: true},
-            {nombre: "Miguel", promedio: "18", estado: true}
         ],
+        nombre: '',
+        promedio: ''
+    },
+
+    methods: {
+        agregarNota: function () {
+            estadoA = false;
+            if(this.promedio < 12.5){
+                estadoA = true;
+            }   
+            
+            if(this.nombre != "" && this.promedio != ""){
+                this.lista.push({nombre: this.nombre,  promedio:this.promedio, estado: this.estadoA});
+                this.nombre = "";
+                this.promedio = "";
+            }else{
+                alert("Ingrese el nombre y el promedio");
+            }
+        }
     }
 })
